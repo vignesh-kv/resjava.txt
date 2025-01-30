77b853a9e7c75a23cc7f7e28c72cbdabcc6b8626
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-30 20:40:49 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,11 @@
+public class input{
+public static void main(String[]args){
+int a=15;
+int b=30;
+if(b>a){
+System.out.println("max value is b");
+}else{
+System.out.println("min value is a");
+}
+}
+}
