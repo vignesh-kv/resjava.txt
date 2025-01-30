85ef1c8992264172d31c0cbacaf01a68442aba41
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-30 20:14:21 +0530

    Add files via upload

---
+++ data.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class data{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int i=sc.nextInt();
+String s=sc.next();
+float f=sc.nextfloat();
+long l=sc.nextlong();
+}
+}
